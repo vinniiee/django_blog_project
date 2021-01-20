f090327e448f5ed2824371e337863003917fe545
vinniiee <vs28245@gmail.com>
2021-01-20 06:18:00 +0530

    configured views and urls

--- django_blog_project/django_blog_project/urls.py
+++ django_blog_project/django_blog_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djago.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'',include('blog_app.urls')),
 ]
